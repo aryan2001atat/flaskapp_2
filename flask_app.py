0da18baa56f4e4c947cea57036dcd7ebc3531de8
aryan2001atat <aryan2001atat@gmail.com>
2024-12-26 16:02:31 +0530

    trying to remove error

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "Hello Im Flask_App_2."
+	return "Welcome to Kubernetes."
 
-if __name__ =="__main__":
-	app.run(host = "0.0.0.0", port=7000, debug = True) 
+if __name__ == "__main__":
+	app.run(host = '0.0.0.0', port=7000, debug = True) 
